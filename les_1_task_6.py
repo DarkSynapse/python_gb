863282f312ac8cae2434a809989f087ca2a28683
Nikita <zac-nikita@yandex.ru>
2020-02-16 18:44:20 +0300

    Create les_1_task_6.py

---
+++ les_1_task_6.py
@@ -0,0 +1,6 @@
+# Девятая задача
+# https://drive.google.com/file/d/1vC5xwU1XavUSfoW9orOjBvO60uMzI-GR/view?usp=sharing
+
+x = int(input('Введите № буквы: '))
+x = ord('a') + x - 1
+print('Ваш буква - ', chr(x))
